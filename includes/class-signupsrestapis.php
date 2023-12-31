d85918e4ddb8447c1c9f4a0a44b2a6d1d24a3817
ecsproull <ecsproull@outlook.com>
2023-12-31 09:58:41 -0700

    Added text forwarding and storage

--- includes/class-signupsrestapis.php
+++ includes/class-signupsrestapis.php
@@ -9,6 +9,8 @@
  * @license     GPL-2.0+
  */
 
+use Twilio\Rest\Client;
+
 /**
  * Create the database tables on activation.
  */
@@ -29,7 +31,7 @@ class SignUpsRestApis extends SignUpsBase {
 					'get_member',
 					$this,
 					array(
-						'badge' => array(
+						'From' => array(
 							'description'       => esc_html( 'Member badge number' ),
 							'type'              => 'string',
 							'validate_callback' => array( $this, 'verify_badge_param' ),
@@ -39,6 +41,78 @@ class SignUpsRestApis extends SignUpsBase {
 				);
 			}
 		);
+
+		add_action(
+			'rest_api_init',
+			function () {
+				$this->register_route(
+					'scwmembers/v1',
+					'/text',
+					'recieve_text',
+					$this,
+					array(
+						'badge' => array(
+							'description'       => esc_html( 'Endpoint for text messages' ),
+							'type'              => 'string',
+							'validate_callback' => array( $this, 'verify_phone_number' ),
+						),
+					),
+					WP_REST_Server::ALLMETHODS
+				);
+			}
+		);
+	}
+
+	/**
+	 * Endpoint for twilio to post a text message.
+	 *
+	 * @param  mixed $request Parameters for the request.
+	 * @return void
+	 */
+	public function recieve_text( $request ) {
+		global $wpdb;
+		$data    = $request->get_params();
+		$sid    = getenv( "TWILIO_ACCOUNT_SID" );
+		$token  = getenv( "TWILIO_AUTH_TOKEN" );
+		$twilio = new Client( $sid, $token );
+		if ( $data['AccountSid'] !== $sid  || ! $data['From'] ) {
+			return;
+		}
+
+		$phone_number = preg_replace( '~.*(\d{3})[^\d]{0,7}(\d{3})[^\d]{0,7}(\d{4}).*~', '$1-$2-$3', $data['From'] );
+		$now          = new DateTime( 'now', new DateTimeZone( 'America/Phoenix' ) );
+		$insert_data  = array(
+			'text_body'       => esc_html( $data['Body'] ),
+			'text_from_phone' => $phone_number,
+			'text_date_time'  => $now->format( self::DATETIME_FORMAT ),
+		);
+
+		$wpdb->insert( self::TEXT_TABLE, $insert_data );
+
+		$server       = 'WC_SERVER\\SQLEXPRESS';
+		$database     = 'WoodClub';
+		$username     = 'memberapp';
+		$password     = 'member';
+		$handle       = new PDO( "sqlsrv:Server=$server;Database=$database;", $username, $password );
+		$handle->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+		$statement = $handle->prepare( "SELECT Badge, FirstName, LastName, Email from MemberRoster WHERE Phone = '$phone_number'" );
+		$result    = $statement->execute();
+		if ( $result ) {
+			$all = $statement->fetchAll( PDO::FETCH_ASSOC );
+			if ( $all ) {
+				$message = $twilio->messages->create(
+					'+14253513207',
+					[
+						"body" => $data['From'] . ' - ' . $all[0]['Badge'] . ' - ' . $all[0]['FirstName'] . ' - ' . $all[0]['LastName'] . ' - ' . $all[0]['Email'] . ' Msg:' . $data['Body'],
+						"from" => '+16233049716'
+					]
+				);
+			}
+		}
+	}
+
+	public function verify_phone_number( $data ) {
+		return true;
 	}
 
 	/**
